59db30370f2d470641e2860618e3feb0b7fe77aa
Sansbend11111 <95498437+Sansbend11111@users.noreply.github.com>
2022-01-06 03:12:29 +0300

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -46,7 +46,6 @@ class MainMenuState extends MusicBeatState
 	var camFollow:FlxObject;
 	var camFollowPos:FlxObject;
 	var debugKeys:Array<FlxKey>;
-	private var char1 Character = null;
 
 	override function create()
 	{
@@ -128,10 +127,6 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-char1 = new Character(800,-130,'bf',true,);
-char1.setGraphicSize(Std.int(char1.width * 0.8));
-add(char1);
-char1.visible = false;
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
@@ -185,20 +180,7 @@ char1.visible = false;
 
 		var lerpVal:Float = CoolUtil.boundTo(elapsed * 7.5, 0, 1);
 		camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
-		
-if (optionShit[curSelected] == 'story_mode')
-{
-    changeItem(-1);
-    changeItem(1);
-    
-    char1.dance();
-    char1.updateHitbox();
-    char1.visible = true;
-}
-else
-{
-    char1.visible = false;
-}
+
 		if (!selectedSomethin)
 		{
 			if (controls.UI_UP_P)
